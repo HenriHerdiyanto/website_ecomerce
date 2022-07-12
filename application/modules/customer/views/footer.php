e190f2b2d4285a3762726a08c125c45785807ee0
HenriHerdiyanto <henri.herdiyanto@students.amikom.ac.id>
2022-07-12 14:17:45 +0700

    bismillah

---
+++ application/modules/customer/views/footer.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+  
+
+  <aside class="control-sidebar control-sidebar-dark">
+  </aside>
+</div>
+
+<script src="<?php echo get_theme_uri('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js', 'adminlte'); ?>"></script>
+<script src="<?php echo get_theme_uri('js/adminlte.js', 'adminlte'); ?>"></script>
+
+<!-- Elapsed in {elapsed_time} times  -->
+</body>
+</html>
